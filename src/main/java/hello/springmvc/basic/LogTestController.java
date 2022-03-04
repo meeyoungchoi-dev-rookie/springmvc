347fb0948a7db4a4399d689225cf21cdcb3519e3
meeyoungchoi <42866800+meeyoungchoi@users.noreply.github.com>
2022-03-04 13:28:49 +0900

    초기 커밋

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogTestController {
 
     // @Slf4j 어노테이션을 사용하면 Logger 객체를 직접 생성하지 않아도 된다
-   //  private  final Logger log = LoggerFactory.getLogger(getClass());
+    //  private  final Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/log-test")
     public String logTest() {
